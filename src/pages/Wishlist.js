8fa30d18a8f324f6a2597791b7d48e1c5fc2326f
Philip Jansson <90768022+Gitphil93@users.noreply.github.com>
2022-09-15 12:47:09 +0200

    wishlist

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer'
+import "../styles/Wishlist.css"
 
 export default function Wishlist() {
   return (
     <div>
-        <h1>This is Wishlist page</h1>
+        <Navbar />
+    <div className="wishlistHeader">
+      <h1>Wishlist</h1>
+      <div/>
+      <div className="wishlistContainer">
+          <ul>
+            <li>Product: Color: Artist: Prod nr: 428539 129 USD <button>Add</button></li>
+            <li>Product: Color: Artist: Prod nr: 428539 129 USD <button>Add</button></li>
+            <li>Product: Color: Artist: Prod nr: 428539 129 USD <button>Add</button></li>
+            <li>Product: Color: Artist: Prod nr: 428539 129 USD <button>Add</button></li>
+            <li>Product: Color: Artist: Prod nr: 428539 129 USD <button>Add</button></li>
+            <li>Product: Color: Artist: Prod nr: 428539 129 USD <button>Add</button></li>
+          </ul>
+      </div>
+      <Footer />
     </div>
+    </div>
+
+
   )
 }
