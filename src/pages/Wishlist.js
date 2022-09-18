b4eaa2f5a61f16c1dff346c48ce3553165fc1e11
Rawisha <rawvsmaw22@gmail.com>
2022-09-18 22:25:21 +0200

    news fixed

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -1,44 +1,3 @@
-import React from 'react'
-<<<<<<< HEAD
-import "../styles/Wishlist.css"
-
-import bild from "../assets/bild.jpg"
-
-export default function Wishlist() {
-  return (
-    <div className='wishlistContainer'>
-
-        <h1>Wishlist</h1>
-
-        <div className='wishlistProductWrapper'>
-          
-          <div>
-            <img id='thumbnail' src={bild} alt="product "></img>
-          </div>
-          <div>
-            <h3>Product: Wait here</h3>
-            <p>Prod nr: 428539</p>
-            <h3>Artist: Stefan Marxx</h3>
-          </div>
-          <div>
-            <h3>Color: Black</h3>
-          </div>
-          <div>
-            <p>2</p>
-            <button>+</button>
-            <button>-</button>
-          </div>
-          <div>
-            <h3>129.00 USD</h3>
-          </div>
-          <div>
-            <button>Add</button>
-          </div>
-          <div>
-            <button>X</button>
-          </div>
-        </div>
-=======
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer'
 import "../styles/Wishlist.css"
@@ -50,6 +9,8 @@ export default function Wishlist() {
     <div className="wishlistHeader">
       <h1>Wishlist</h1>
       <div/>
+      <div className='wishlistWrapper'>
+
       <div className="wishlistContainer">
           <ul>
             <li>Product: Color: Artist: Prod nr: 428539 129 USD <button>Add</button></li>
@@ -60,8 +21,9 @@ export default function Wishlist() {
             <li>Product: Color: Artist: Prod nr: 428539 129 USD <button>Add</button></li>
           </ul>
       </div>
+      </div>
       <Footer />
->>>>>>> 8fa30d18a8f324f6a2597791b7d48e1c5fc2326f
+
     </div>
     </div>
 
