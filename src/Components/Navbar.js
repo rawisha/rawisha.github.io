6937f5c9c7049843a8199c1fb47bd214aa137149
Anton Thunberg <anton@thunberg.nu>
2022-09-23 14:34:23 +0200

    fix search

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -32,6 +32,7 @@ export default function Navbar() {
 
         const [value, setValue] = useState("");
         const [results, setResults] = useState([]);
+        const [prodResulsts, setProdResolts] = useState([]);
 
         const onChange = (event) => {
             setValue(event.target.value)
@@ -50,7 +51,12 @@ export default function Navbar() {
                 ))
             })
         }, [])
-        /* console.log('artist', results) */
+        useEffect(() => {
+            onSnapshot(collection(db, 'products'), snapshot => {
+                setProdResolts(snapshot.docs.map(doc => doc.data()
+                ))
+            })
+        }, [])
 
     return (
         <div className='Navbar'>
@@ -106,7 +112,33 @@ export default function Navbar() {
                 {item.artistname}
               </div>
             ))}
+            
                 </div>
+                <div className='dataResult'>
+                    {prodResulsts
+                    .filter((item) => {
+                        const searchTerm = value;
+                        const prodName = item.title;
+                        
+                        return (
+                            searchTerm && 
+                            prodName.startsWith(searchTerm) &&
+                            prodName != searchTerm
+                        );
+                    })
+                    .slice(0, 10)
+                    .map((item) => (
+                        <div
+                            onClick={() => onSearch(item.title)}
+                            className="dropdown-row"
+                            key={item.title}
+                        >
+                            {item.title}
+                        </div>
+                    ))
+                    }
+                </div>
+                
         </div>
     )
 }
