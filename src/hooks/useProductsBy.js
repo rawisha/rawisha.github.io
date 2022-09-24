64f207980d60f0f164800e203af6085a382e1e7b
davidobrant <david.obrant@gmail.com>
2022-09-24 10:44:29 +0200

    backend-fix

--- src/hooks/useProductsBy.js
+++ src/hooks/useProductsBy.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { onSnapshot, collection, orderBy, where, query } from 'firebase/firestore'
+import { onSnapshot, collection, where, query } from 'firebase/firestore'
 import { db } from '../firebase-config'
 
 const useProductsBy = (categoryRef) => {
