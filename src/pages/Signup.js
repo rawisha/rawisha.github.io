73ed131157cfe542b9c915689d419d7de0e6ac39
Maria Toledo <maria.toledo@live.se>
2022-09-24 11:41:32 +0200

    artist register and almost upload
    artist register after checkbox click, not done yet with upload

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -3,22 +3,90 @@ import Navbar from '../Components/Navbar'
 import "../styles/Signup.css"
 import Footer from '../Components/Footer'
 
-import { signup, db, emailVerification } from '../firebase-config'
+import { signup, db, emailVerification,storage } from '../firebase-config'
 import { useRef, useState } from 'react'
-import { doc , setDoc, serverTimestamp} from 'firebase/firestore'
+import { doc , setDoc, serverTimestamp, refEqual} from 'firebase/firestore'
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage"
+
 
 export default function Signup() {
-
-  const [loading, setLoading] = useState(false);
 
   const emailRef = useRef();
   const passwordRef = useRef();
   const firstNameRef = useRef();
   const lastNameRef = useRef();
   const userNameRef = useRef();
+  const artistNameRef = useRef();
+  const textAreaTextRef = useRef();
+
+  const [loading, setLoading] = useState(false);
+  const [checked, setChecked] = useState(false);
+  const [urls, setUrls] = useState([])
+  // State to store uploaded file
+  const [images, setImages] = useState([]);
+  // progress
+  const [percent, setPercent] = useState(0);
+  const [progress, setProgress] = useState(0);
+
+
+  // Handle files upload event and update state
+  const handleChange = (e) => {
+    for (let i = 0; i < e.target.files.length; i++) {
+      const newImage = e.target.files[i];
+      newImage["id"] = Math.random();
+      setImages((prevState) => [...prevState, newImage]);
+    }
+  };
+
+  const  handleUpload = async () => {
+    const promises = [];
+    images.map((image) => {
+      const storageRef = ref(storage,`testing/${image.name}`).put(image);
+      const uploadTask = uploadBytesResumable(storageRef, image);
+
+      promises.push(uploadTask);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const percent = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setPercent(percent);
+        },
+        (error) => {
+          console.error(error);
+        },
+        async () => {
+          await storage
+            .ref("testing")
+            .child(image.name)
+            .getDownloadURL()
+            .then((urls) => {
+              setUrls((prevState) => [...prevState, urls]);
+            });
+        }
+      );
+    )
+    });
+
+    Promise.all(promises)
+      .then(() => alert("All images uploaded"))
+      .catch((err) => console.error(err));
+  };
+
+  console.log("images: ", images);
+  console.log("urls: ", urls);
+
+
+  function showApplicationArtist(){
+    setChecked((checked) => !checked);
+  }
+
   
 
-  async function handleSignup(){
+
+  async function handleSignupUser(){
     setLoading(true);
     try{
       await signup(emailRef.current.value, passwordRef.current.value).then (cred =>{
@@ -39,10 +107,34 @@ export default function Signup() {
     setLoading(false);
   }
 
+  async function handleSignupArtist(){
+    setLoading(true);
+    try{
+      await signup(emailRef.current.value, passwordRef.current.value).then (cred =>{
+        setDoc(doc(db, "artists", cred.user.uid), {
+          firstName : firstNameRef.current.value,
+          lastName : lastNameRef.current.value,
+          artistName : artistNameRef.current.value,
+          eMail : emailRef.current.value,
+          bio: textAreaTextRef.current.value,
+          timestamp : serverTimestamp() 
+        })
+        emailVerification();
+      })
+    } catch (error){
+      console.error(error);
+    }
+    setLoading(false);
+  }
+
+
+
+
+
+
   return (
     <div>
       <Navbar />
-  
       <div className="signupForm">
         <h1>Register</h1>
         <input type= "text" ref ={firstNameRef} placeholder="First Name"></input>
@@ -50,11 +142,39 @@ export default function Signup() {
         <input type= "text" ref ={userNameRef} placeholder="Username"></input>
         <input type = "email" ref = { emailRef }placeholder="Email" required></input>
         <input type = "password" ref= { passwordRef } placeholder="Password (8 Digits)" minLength="8" required></input>
-        {/* <label><input type="checkbox"></input>Artist Application? Check this box to apply</label>  */}
-        <button disabled = {loading} onClick={handleSignup}>Create</button>
-        </div>
+        <button disabled = {loading} onClick={handleSignupUser}><h2>Create</h2></button>
         
-        <Footer />
+      <div className="checkboxDiv">
+        <input type ="checkbox" id="checkbox" onClick={showApplicationArtist}></input>
+        <p id="checkboxParagraph">Artist application? Check this box to apply</p>
+      </div>
+      
+      <div style={{ display: checked ? "block" : "none" }} className="applicationDiv">
+        <div className="signupForm">
+        <h1>Artist Application</h1>
+        <p>Let us know more about you, please upload pictures of your work and tell us about yourself</p>
+        <input type= "text" ref ={firstNameRef} placeholder="First Name"></input>
+        <input type= "text" ref ={lastNameRef} placeholder="Last Name"></input>
+        <input type= "text" ref ={artistNameRef} placeholder="Artist Name"></input>
+        <input type = "email" ref = { emailRef }placeholder="Email" required></input>
+        <input type = "password" ref= { passwordRef } placeholder="Password (8 Digits)" minLength="8" required></input>
+        </div>
+
+        <div className="uploadForm"> 
+        <input type="file" accept='image/*'onChange={handleChange} multiple/>
+        <button disabled = {loading} onClick={handleSignupArtist}><h3>Register and upload</h3></button>
+
+        <button onClick={handleUpload}><h3>Upload to Firebase</h3></button>
+        <p>{percent} % done</p>
+        <h2>Uploading done {progress}%</h2>
+
+        </div>
+        <textarea ref={textAreaTextRef} cols="80" rows="15" placeholder='About you and your work'></textarea>
+        <button disabled = {loading} onClick={handleSignupArtist}><h3>Register and upload images</h3></button>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   )
 }
